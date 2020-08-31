bc47357305d995b8b21738dc2064914157016c8a
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-08-31 14:55:18 -0300

    Added the routes folder / Added the index route

--- lib/server.ts
+++ lib/server.ts
@@ -1,9 +1,9 @@
+// routes
+import index from './routes/index';
+
 import express from 'express';
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/', index);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
